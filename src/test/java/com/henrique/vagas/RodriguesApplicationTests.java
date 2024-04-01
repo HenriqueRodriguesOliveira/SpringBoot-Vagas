bc6fb0bf86de610579ec6aeffc3a9c04306a7cfd
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-04-01 16:28:49 -0300

    first commit

---
+++ src/test/java/com/henrique/vagas/RodriguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodriguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
