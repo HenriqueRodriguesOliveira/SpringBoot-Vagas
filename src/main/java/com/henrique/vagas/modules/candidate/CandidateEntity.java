29b053f91c90126ea3ef105a1a8aed38971a24c7
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-04-10 09:58:56 -0300

    Inserindo JPA

--- src/main/java/com/henrique/vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/henrique/vagas/modules/candidate/CandidateEntity.java
@@ -2,26 +2,29 @@ package com.henrique.vagas.modules.candidate;
 
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-
 import java.util.UUID;
 
-@Data
+@Entity
 public class CandidateEntity {
 
+    @Id
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
     private String description;
     private String curriculum;
