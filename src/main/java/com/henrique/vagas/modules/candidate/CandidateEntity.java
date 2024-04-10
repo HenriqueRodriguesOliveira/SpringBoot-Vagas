7abf16ae1e335b96bdf09ccf0a01fa8d34fe4a9a
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-04-10 16:36:35 -0300

    Criando tabela e repositório

--- src/main/java/com/henrique/vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/henrique/vagas/modules/candidate/CandidateEntity.java
@@ -2,19 +2,25 @@ package com.henrique.vagas.modules.candidate;
 
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
+@Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank()
@@ -28,6 +34,9 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     public UUID getId() {
         return id;
--- src/main/java/com/henrique/vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/henrique/vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package com.henrique.vagas.modules.candidate.controllers;
 
 import com.henrique.vagas.modules.candidate.CandidateEntity;
+import com.henrique.vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
