d24730cd314394f672a6465a6a3969aebcae35bf
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-04-15 10:16:51 -0300

    Entidade Job

---
+++ src/main/java/com/henrique/vagas/modules/company/entites/JobEntity.java
@@ -0,0 +1,29 @@
+package com.henrique.vagas.modules.company.entites;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    private LocalDateTime createdAt;
+
+}
